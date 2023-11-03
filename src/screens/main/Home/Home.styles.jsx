cbbcc04dfee16bfce4a3fc21d7036dba2cf79de8
Fdobrovols <78583045+Fdobrovols@users.noreply.github.com>
2023-11-03 23:45:02 +0200

    push

--- src/screens/main/Home/Home.styles.jsx
+++ src/screens/main/Home/Home.styles.jsx
@@ -11,7 +11,7 @@ export default styles = StyleSheet.create({
   },
   plusBarIcon: {
     display: "flex",
-    justifyContent: "center",
+    justifyContent: "left",
     alignItems: "center",
     width: 70,
     height: 40,
