150c7cc715438d8633d63b2f761a23f5453a33c4
Fdobrovols <78583045+Fdobrovols@users.noreply.github.com>
2023-11-18 21:03:54 +0200

    fix

--- src/screens/main/Home/Home.styles.jsx
+++ src/screens/main/Home/Home.styles.jsx
@@ -13,7 +13,7 @@ export default styles = StyleSheet.create({
     display: "grid",
     justifyContent: "bottom",
     alignItems: "center",
-    width: 60,
+    width: 40,
     height: 40,
     borderRadius: 10,
     backgroundColor: "#FF6C00",
